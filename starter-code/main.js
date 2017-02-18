75caaeb7c192a341fef24685daa173a4d647bd1f
pepelou <louisa.leeyh@gmail.com>
2017-02-18 17:20:24 +0800

    make create cards function

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,20 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again");
+}
+
+var createCards = function() {
+	var board = document.getElementById('game-board');
+	for(var i=0;i<4;i++) {
+		var div = document.createElement('div');
+		div.className = "card";
+		board.appendChild(div);
+	}
+}
+
+createCards();
